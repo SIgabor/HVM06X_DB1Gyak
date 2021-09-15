176c28beb995ceb49059d63b3cb464aad44178ed
SIgabor <79259040+SIgabor@users.noreply.github.com>
2021-09-15 13:17:54 +0200

    Add files via upload

---
+++ HVM06X_0915/SIG2_2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    iras();
+    return 0;
+}
+
+void iras(){
+    FILE *fp;
+    char ch;
+    char fnev[50];
+    printf("Filenev: ");
+    scanf("%s", fnev);
+    fp = fopen(fnev, "w");
+    printf("Uzenet: ");
+    while((ch = getchar()) != '#'){
+        putc(ch, fp);
+    }
+    fclose(fp);
+
+    fp = fopen(fnev, "r");
+    while((ch = getc(fp)) != EOF){
+        printf("%c", toupper(ch));
+    }
+    fclose(fp);
+    return 0;
+}
